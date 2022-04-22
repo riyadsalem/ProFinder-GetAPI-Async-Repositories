bf6db345c21f6fa1bfa39401f6c7a8a021225fda
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-04-22 16:02:29 +0300

    Search Show Users

--- index.js
+++ index.js
@@ -27,5 +27,35 @@ document.getElementById("search").addEventListener("submit", async (e) => {
 
   const profile = await getUser(username);
 
+  showProfile(profile);
   console.log(profile);
 });
+
+function showProfile(profile) {
+  document.querySelector(".profile").innerHTML = `
+<img
+src="${profile.avatar_url}"
+alt="letstrie"
+/>
+<p class="name">${profile.name}</p>
+<p class="username login">${profile.login}</p>
+<p class="bio">${profile.bio}</p>
+
+<div class="followers-stars">
+<p>
+  <ion-icon name="people-outline"></ion-icon>
+  <span class="followers"> ${profile.followers} </span> followers
+</p>
+<span class="dot">·</span>
+<p><span class="following"> ${profile.following} </span> following</p>
+</div>
+
+<p class="company">
+<ion-icon name="business-outline"></ion-icon>
+${profile.company}
+</p>
+<p class="location">
+<ion-icon name="location-outline"></ion-icon>${profile.location}
+</p>
+`;
+}
